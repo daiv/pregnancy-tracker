3114c07b6904379a9deca1e729a030d5ef77954e
David-Torre-Ubuntu <sobacoman@hotmail.com>
2025-02-27 21:37:13 +0100

    created Info component to show the info related with baby and mother per week

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+import Info from './Info';
+import Form from './Form';
+import Baby from './Baby';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [week, setWeek] = useState(2);
+  const currentWeek = 16;
+  return (
+    <div className='app-container'>
+      <Info week={week} />
+      
 
-  return <h1>let's start!!</h1>
+    </div>
+  );
+
 }
 
 export default App
