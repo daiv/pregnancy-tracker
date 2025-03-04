6f55b04a83105965ccb64f95d5aba0d2acb418ac
David-Torre-Ubuntu <sobacoman@hotmail.com>
2025-03-04 22:51:51 +0100

    css styling

--- client/src/components/EventList.jsx
+++ client/src/components/EventList.jsx
@@ -4,7 +4,7 @@ export default function EventList({ eventList, removeEvent }) {
   return (
     <>
       {eventList.length === 0
-        ? <h1>no events yet</h1>
+        ? <h1 className="event-list-container">no events yet</h1>
         : <div className="event-list-container">{eventList.map(el => <EventItem key={el._id} event={el} removeEvent={removeEvent} />)}</div>
       }
     </>
