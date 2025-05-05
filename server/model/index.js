708a3cf002d127cbf4e34dd9e3ebf66d97f41334
David-Torre-Ubuntu <sobacoman@hotmail.com>
2025-05-05 18:14:00 +0200

    add dotenv

--- server/model/index.js
+++ server/model/index.js
@@ -1,7 +1,10 @@
 import { mongoose, connect } from 'mongoose';
+import 'dotenv/config';
+
 
 const URL = 'mongodb://127.0.0.1:27017';
-const DATABASE = 'pregnancy-tracker';
+const DATABASE = (process.env.DATABASE_NAME || 'preg-tracker') + (process.env.NODE_ENV === 'test' ? '_test' : '');
+
 
 mongoose.startConnection = async () => {
   await connect(URL + '/' + DATABASE);
